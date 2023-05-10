249700b977e5c008a5f48f668efe149e49c1b2aa
Felipe Vaccarini <you@example.com>
2023-05-10 20:24:38 -0300

    criando atributos

--- src/main/java/com/example/sce/dominio/Produto.java
+++ src/main/java/com/example/sce/dominio/Produto.java
@@ -1,4 +1,23 @@
 package com.example.sce.dominio;
 
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String nome;
+    private String marca;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date fabricacao;
+
+    private Float preco;
 }
