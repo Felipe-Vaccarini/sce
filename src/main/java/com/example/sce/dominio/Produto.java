60b607b43e73e1c1bf28b457c590bd4cf06d2b0d
Felipe Vaccarini <you@example.com>
2023-05-11 18:55:14 -0300

    getters e setters

--- src/main/java/com/example/sce/dominio/Produto.java
+++ src/main/java/com/example/sce/dominio/Produto.java
@@ -14,10 +14,52 @@ public class Produto {
     private long id;
 
     private String nome;
+
     private String marca;
 
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date fabricacao;
 
     private Float preco;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Date getFabricacao() {
+        return fabricacao;
+    }
+
+    public void setFabricacao(Date fabricacao) {
+        this.fabricacao = fabricacao;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+
 }
