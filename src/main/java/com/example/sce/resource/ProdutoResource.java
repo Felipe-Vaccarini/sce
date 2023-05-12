e52ed1dc38a26c48a0ad0201f08a44251589e348
Felipe Vaccarini <you@example.com>
2023-05-12 20:06:44 -0300

    adicionado @PostMapping

--- src/main/java/com/example/sce/resource/ProdutoResource.java
+++ src/main/java/com/example/sce/resource/ProdutoResource.java
@@ -3,9 +3,9 @@ package com.example.sce.resource;
 import com.example.sce.dominio.Produto;
 import com.example.sce.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -22,4 +22,12 @@ public class ProdutoResource {
     public List<Produto> list() {
         return produtoRepository.findAll();
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<Produto> create(@RequestBody Produto produto) {
+        produto = produtoRepository.save(produto);
+
+        return new ResponseEntity(produto, HttpStatus.OK);
+    }
+
 }
