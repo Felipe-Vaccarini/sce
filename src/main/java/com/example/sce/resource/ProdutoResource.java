2932275189c5df12d51c98e41560e6213de8d6f0
Felipe Vaccarini <you@example.com>
2023-05-12 20:15:21 -0300

    adicionado @PutMapping, @DeleteMapping

--- src/main/java/com/example/sce/resource/ProdutoResource.java
+++ src/main/java/com/example/sce/resource/ProdutoResource.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/produto")
@@ -28,6 +28,29 @@ public class ProdutoResource {
         produto = produtoRepository.save(produto);
 
         return new ResponseEntity(produto, HttpStatus.OK);
+    }
+
+    @GetMapping("getById/{id}")
+    public Optional<Produto> getById(@PathVariable(value = "id") int id) {
+        return produtoRepository.findById(id);
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Produto> editar(@RequestBody Produto produto) {
+        produto = produtoRepository.save(produto);
+        return new ResponseEntity(produto, HttpStatus.OK);
+    }
+
+    @GetMapping("/total")
+    public long getTotal() {
+        return produtoRepository.count();
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public Produto remove(@PathVariable(value = "id") int id) {
+        Optional<Produto> optionalProduto = produtoRepository.findById(id);
+        produtoRepository.delete(optionalProduto.get());
+        return optionalProduto.get();
     }
 
 }
